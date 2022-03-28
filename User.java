feb946d3486605f09636478cce7174adc8751ab5
mfrag2025 <59969280+mfrag2025@users.noreply.github.com>
2022-03-28 12:27:20 +0530

    Update User.java
    Added setter and getter methods

--- User.java
+++ User.java
@@ -7,5 +7,29 @@ public class User {
     private String password;
  
     // getters and setters are not shown for brevity   
+   public int getId() {
+    return id;
+   }
+   public void setId(int id) {
+    this.id = id;
+   }
+   public String getFullname() {
+    return fullname;
+   }
+   public void setFullname(String fullname) {
+    this.fullname = fullname;
+   }
+   public String getEmail() {
+    return email;
+   }
+   public void setEmail(String email) {
+    this.email = email;
+   }
+   public String getPassword() {
+    return password;
+   }
+   public void setPassword(String password) {
+    this.password = password;
+   }
      
 }
