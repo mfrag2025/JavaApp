310577b5dad11da9571b90281963320461482606
mfrag2025 <59969280+mfrag2025@users.noreply.github.com>
2022-03-28 02:28:02 -0400

    Add files via upload

---
+++ User.java
@@ -0,0 +1,11 @@
+package net.codejava;
+ 
+public class User {
+    private int id;
+    private String fullname;
+    private String email;
+    private String password;
+ 
+    // getters and setters are not shown for brevity   
+     
+}
